42519797e705ae81007774b81d6579d09648df2f
RabiaMehreen <165903310+RabiaMehreen@users.noreply.github.com>
2024-09-28 01:54:29 -0700

    Style.js

---
+++ Style.js
@@ -0,0 +1,28 @@
+// Calculator JS-code
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+let string = "";
+let arr = Array.from(buttons);
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string;
+        }
+        // Clear all inputs
+        else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = string;
+        }
+        // delete the input value
+        else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        // To show the input values
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+})
